229e03a905153003351c983ee58c629935453614
Elyzaveta Beregovenko <lizakibich@gmail.com>
2023-01-12 14:00:52 +0200

    HW-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// Реалізація делегування на div.gallery і отримання url великого зображення.
+// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
 
 console.log(galleryItems);
+const galleryDiv = document.querySelector('.gallery');
+const galleryCard = createGalleryCards(galleryItems);
+galleryDiv.insertAdjacentHTML('afterbegin', galleryCard);
+
+function createGalleryCards(images) {
+  return images
+    .map(
+      image =>
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${image.original}" onclick="return false;">
+            <img
+                class="gallery__image"
+                src="${image.preview}"
+                data-source="${image.original}"
+                alt="${image.description}"
+            />
+            </a>
+            </div>
+        `
+    )
+    .join('');
+}
+
+// import * as basicLightbox from 'basiclightbox'
+
+galleryDiv.addEventListener('click', openImage);
+
+function openImage(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`);
+  instance.show();
+  galleryDiv.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
