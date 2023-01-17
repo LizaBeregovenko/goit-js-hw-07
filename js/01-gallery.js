414df7e766b395ddfa7ad63137dee8d28a5208a7
Elyzaveta Beregovenko <lizakibich@gmail.com>
2023-01-17 14:57:44 +0200

    HW7-0/2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,4 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
-
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// Реалізація делегування на div.gallery і отримання url великого зображення.
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-
 console.log(galleryItems);
 const galleryDiv = document.querySelector('.gallery');
 const galleryCard = createGalleryCards(galleryItems);
@@ -52,3 +43,55 @@ function openImage(event) {
     }
   });
 }
+// const gallery = document.querySelector('.gallery');
+
+// const markup = galleryItems
+//   .map(
+//     ({ preview, description, original }) => `<div class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </div>`
+//   )
+//   .join('');
+
+// gallery.insertAdjacentHTML('beforeend', markup);
+// gallery.addEventListener('click', onGalleryClick);
+
+// function onGalleryClick(e) {
+//   e.preventDefault();
+
+//   if (e.target.nodeName !== 'IMG') {
+//     return;
+//   }
+
+//   const instance = basicLightbox.create(
+//     `
+//            <img
+//            class="gallery__image"
+//            src="${e.target.dataset.source}"
+//            data-source="${e.target.dataset.source}"
+//            alt="${e.target.alt}"/>`,
+//     {
+//       onShow: () => {
+//         window.addEventListener('keydown', onModalCloseByEscape);
+//       },
+//       onClose: () => {
+//         window.removeEventListener('keydown', onModalCloseByEscape);
+//       },
+//     }
+//   );
+
+//   instance.show();
+
+//   function onModalCloseByEscape(e) {
+//     if (e.code === 'Escape') {
+//       instance.close();
+//     }
+//   }
+// }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -30,3 +30,30 @@ const ligthbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
 });
 //
+// const gallery = document.querySelector('.gallery');
+
+// const markup = galleryItems
+//   .map(
+//     ({ preview, description, original }) => `<li>
+//     <a class="gallery__item" href="${original}">
+//       <img
+//         class="gallery__image"
+//         src="${preview}"
+//         alt="${description}"
+//       />
+//     </a></li>`
+//   )
+//   .join('');
+
+// gallery.insertAdjacentHTML('beforeend', markup);
+
+// const arrLinks = document.querySelectorAll('a');
+// arrLinks.forEach(link => {
+//   link.style.boxShadow = 'none';
+// });
+
+// new SimpleLightbox('ul.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+//   captionPosition: 'bottom',
+// });
